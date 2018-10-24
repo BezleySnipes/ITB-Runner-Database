a2b53a1a36bf3a7cf494d56abe2d357e3bc595a0
BezleySnipes <David.Mulhall@gmail.com>
2018-10-24 22:17:01 +0100

    Added Test class to test connection to data base

---
+++ itbRunner/src/itbRunner/test.java
@@ -0,0 +1,12 @@
+package itbRunner;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		runner r1 = new runner();
+		r1.add("dave","Home",16);
+
+	}
+
+}
