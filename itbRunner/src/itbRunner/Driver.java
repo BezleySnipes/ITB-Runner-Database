5362960a1302762a15cc7655ad9175718afd1b7f
BezleySnipes <David.Mulhall@gmail.com>
2018-10-24 22:15:32 +0100

    Added Driver class

---
+++ itbRunner/src/itbRunner/Driver.java
@@ -0,0 +1,34 @@
+package itbRunner;
+
+import java.sql.*;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+
+			// get a connection to database
+			Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/ITB", "root", "root");
+
+			// create statement
+			Statement myState = myConn.createStatement();
+
+			// execute sql query
+			// int numRecordsUpdated = myState.executeUpdate("INSERT INTO dept VALUES (22,'abc','ghjk')");
+			myState.execute("INSERT INTO dept VALUES (22,'abc','ghjk')");
+			// System.out.println(numRecordsUpdated);
+			ResultSet myRs = myState.executeQuery("select ename,sal from emp where sal between 1500 and 3000");
+			// process the result set
+			while (myRs.next()) {
+				System.out.println(myRs.getString("ENAME") + " , " + myRs.getString("SAL"));
+			}
+			
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
+
+}
